8cd0968f0fe329b20e1c227b7c1099342ceac71f
Kajamugesh Raneethran <mugesh.raneethran@mail.utoronto.ca>
2023-10-31 20:54:54 -0400

    add ui to game with score checking and buttons

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let score = 0;
+let comScore = 0;
+
 function getComputerChoice() {
 
   let rand_num = Math.floor(Math.random() * 3)
@@ -19,6 +22,8 @@ function getComputerChoice() {
 function result(playerSelection, computerSelection) {
 
   let result = "placeholder text"
+  const scoreElem = document.querySelector('.Score');
+
 
   if(playerSelection == computerSelection){
     result = "It's a Draw!"
@@ -28,9 +33,13 @@ function result(playerSelection, computerSelection) {
           || (playerSelection == "Scissors" && computerSelection == "Paper")
         ){
     result = `You Win! ${playerSelection} beats ${computerSelection}`
+    score++;
+    scoreElem.textContent = `Your Score: ${score} Computer Score:  ${comScore}`;
   }
   else{
     result = `You Lose! ${computerSelection} beats ${playerSelection}`
+    comScore++;
+    scoreElem.textContent = `Your Score: ${score} Computer Score:  ${comScore}`;
   }
   return result;
 }
@@ -42,10 +51,26 @@ function playRound(playerSelection, computerSelection) {
   return result (playerSelection, computerSelection);
 }
 
-function game(){
-  console.log(playRound(prompt(),getComputerChoice()));
-  console.log(playRound(prompt(),getComputerChoice()));
-  console.log(playRound(prompt(),getComputerChoice()));
-  console.log(playRound(prompt(),getComputerChoice()));
-  console.log(playRound(prompt(),getComputerChoice()));
+function game(choice){
+  const resElem = document.querySelector('.Result');
+  resElem.textContent = playRound(choice,getComputerChoice());
+  const winElem = document.querySelector('.Winner');
+  if (score == 5){
+    winElem.textContent = "Congrats you win the game!"
+  }
+  else if (comScore == 5){
+    winElem.textContent = "Game Over! Computer wins!"
+  }
 }
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    game(button.name);
+  });
+});
